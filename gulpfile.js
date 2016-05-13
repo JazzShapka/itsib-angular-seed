0975e930d7645f4f4108fab73703eac45d3f3451
Sergey <itsib.su@gmail.com>
2016-05-13 08:30:55 +0700

    Настроил модуль gulp-scss

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,22 @@
 //ngAnnotate - проставляем ag зависимости
 //uglify     - минификация в одну строку
 
-var gulp        = require('gulp');
+var gulp        = require('gulp'),
+    sass        = require('gulp-sass');
 
     //concat      = require('gulp-concat'),
     //ngAnnotate  = require('gulp-ng-annotate'),
     //uglify      = require('gulp-uglify');
+
+gulp.task('sass', function () {
+    return gulp.src('./app/assets/scss/style.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./app/build/css'))
+});
+
+gulp.task('sass:watch', function () {
+    gulp.watch('./app/assets/scss/style.scss', ['sass']);
+});
 
 //gulp.task('build-js', function() {
     //return gulp.src(['./app/**/*.js', '!./app/min_js/*.js'])

c5d52c72e9ca751f2428af7900dcb65220d8c281
Sergey <itsib.su@gmail.com>
2016-05-15 22:44:16 +0700

    Добавил модулей для сжатия javascript

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,24 @@
+//sass       - плагин для сборки scss.
+
+//imagemin   - Плагин для сжатия изображений
+
 //concat     - склеиваем js файлы в один
 //ngAnnotate - проставляем ag зависимости
 //uglify     - минификация в одну строку
 
 var gulp        = require('gulp'),
-    sass        = require('gulp-sass');
+    sass        = require('gulp-sass'),
+    imagemin    = require('gulp-imagemin'),
 
-    //concat      = require('gulp-concat'),
-    //ngAnnotate  = require('gulp-ng-annotate'),
-    //uglify      = require('gulp-uglify');
+    concat      = require('gulp-concat'),
+    ngAnnotate  = require('gulp-ng-annotate'),
+    uglify      = require('gulp-uglify');
+
 
 gulp.task('sass', function () {
     return gulp.src('./app/assets/scss/style.scss')
-        .pipe(sass().on('error', sass.logError))
+        // Если не передовать объект
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
         .pipe(gulp.dest('./app/build/css'))
 });
 
@@ -19,11 +26,17 @@ gulp.task('sass:watch', function () {
     gulp.watch('./app/assets/scss/style.scss', ['sass']);
 });
 
-//gulp.task('build-js', function() {
-    //return gulp.src(['./app/**/*.js', '!./app/min_js/*.js'])
-        //.pipe(concat('tim_js.js'))
-        //.pipe(ngAnnotate())
-        //.pipe(uglify())
-        //.pipe(gulp.dest('./app/min_js'));
-//});
+gulp.task('compression-img', function(){
+    gulp.src('./app/assets/images/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./app/build/images'))
+});
+
+gulp.task('build-js', function() {
+    return gulp.src(['./app/**/*.js', '!./app/build/js/*.js'])
+        .pipe(concat('script.min.js'))
+        .pipe(ngAnnotate())
+        .pipe(uglify())
+        .pipe(gulp.dest('./app/build/js'));
+});
 
